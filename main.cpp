405b32537105d823d9a304702d6614a9ae6069f8
aybweq <ayberkcevik654@gmail.com>
2026-04-18 16:19:31 +0300

    genMaze completed

--- main.cpp
+++ main.cpp
@@ -5,26 +5,16 @@
 #include <random>
 #include <algorithm>
 
-// # -> wall
-// ' ' -> empty space
-// $ -> player
 
-typedef enum Difficulty {
-	EASY = 1,
-	NORMAL,
-	HARD,
-	CUSTOM
-} Difficulty;
-
-bool isAvailable(int width, int height, int nx, int ny, std::vector<std::vector<bool>> visited) {
+bool isAvailable(int width, int height, int nx, int ny, const std::vector<std::vector<bool>>& visited) {
 
 	if(nx >= 0 && nx < width && ny >= 0 && ny < height) {
-		if(visited[nx][ny] == false) {
+		if(visited[ny][nx] == false) {
 			return true;
 		}
 	}
 
-	return false
+	return false;
 
 }
 
@@ -40,16 +30,15 @@ std::vector<std::vector<char>> generateMaze(int width, int height) {
 
 
 	std::vector<std::pair<int, int>> Directions {
-		{0, 1}, 	// right
-		{0, -1}, 	// left
-		{1, 0},	 // down
-		{-1, 0} 	// up
+		{1, 0}, 	// right
+		{-1, 0}, 	// left
+		{0, 1},	 // down
+		{0, -1} 	// up
 	};
 
 	// generating random number
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::shuffle(Directions.begin(), Directions.end(), gen);
 
 
 	// starting point
@@ -57,11 +46,15 @@ std::vector<std::vector<char>> generateMaze(int width, int height) {
 	stack.push({0, 0});
 
 	// tracking visited cells
-	std::vector<std::vector<bool>> visited {height, std::vector<bool>(width, false)};
+	std::vector<std::vector<bool>> visited (height, std::vector<bool>(width, false));
 	visited[0][0] = true;
 
 	std::pair<int, int> randomDir;
 
+
+	// shuffle olayini duzelt
+	// stack push ve pop olayini hallet
+	// hangi cell'ler yola cevrilecek?
 
 	while(!stack.empty()) {
 
@@ -69,28 +62,39 @@ std::vector<std::vector<char>> generateMaze(int width, int height) {
 		int cx {stack.top().first};
 		int cy {stack.top().second};
 
-		// a random direction selected
-		randomDir = Directions[generator(gen)];
+		// shuffling 
+		std::shuffle(Directions.begin(), Directions.end(), gen);
 
-		// next coordinates
-		int nx {cx + randomDir.first * 2};
-		int ny {cy + randomDir.second * 2};
+		bool foundNeighbour = false;
 
+		for(const auto& dir: Directions) {
 
-		while(!isAvailable(width, height, nx, ny, visited)) {
-			randomDir = Directions[generator(gen)];
-			int nx {cx + randomDir.first * 2};
-			int ny {cy + randomDir.second * 2};
+			// next coordinates
+			int nx {cx + dir.first * 2};
+			int ny {cy + dir.second * 2};
+
+			if(isAvailable(width, height, nx, ny, visited)) {
+
+				// middle cell
+				int mx {(cx + nx)/2};
+				int my {(cy + ny)/2};
+
+				// convert cells to road
+				maze[my][mx] = ' ';
+				maze[ny][nx] = ' ';
+
+				visited[ny][nx] = true;
+
+				stack.push({nx, ny});
+				foundNeighbour = true;
+
+				break;
+			}
 		}
 
-		// opening the way
-		int mx = (cx + nx)/2;
-		int my = (cy + ny)/2;
-		maze[mx][my] = ' ';
-		maze[nx][ny] = ' ';
-
-
-		stack.push({nx, ny});
+		if(!foundNeighbour) {
+			stack.pop();
+		}
 
 	}
 	
@@ -133,34 +137,7 @@ bool isGameWon() {
 
 int main() {
 
-	Difficulty difficulty;
-	int diffSelection;
-
-	std::cout << "Difficulty\n";
-	std::cout << "1- 21x21\t 150seconds\n";
-	std::cout << "2- 31x31\t 120seconds\n";
-	std::cout << "3- 41x41\t 90seconds\n";
-
-	std::cin >> diffSelection;
-	difficulty = static_cast<Difficulty>(diffSelection);
-
-
-
-	switch (difficulty) {
-
-		case EASY:
-			generateMaze(21, 21);
-
-		case NORMAL:
-			generateMaze(31, 31);
-		
-		case HARD:
-			generateMaze(41, 41);
-
-	}
-
-	while(!isGameWon) {
-
-	}
+	auto maze = generateMaze(21, 21);
+	printMaze(maze);
 
 }
